1e22b626f30b90dc165ec29a6b51360830d594ef
Preston Holmes <ptone@google.com>
2020-08-12 21:24:53 +0000

    new message with typo

--- sample-go/hello.go
+++ sample-go/hello.go
@@ -23,7 +23,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "hello, world from continuous delivery")
+		fmt.Fprint(w, "hello, Aparn")
 	})
 
 	port := os.Getenv("PORT")
